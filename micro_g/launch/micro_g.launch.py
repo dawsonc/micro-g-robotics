fa38caefa34bb3dab783a06e126b819b266d0473
Evan Palmer <evanp922@gmail.com>
2023-10-09 18:42:04 -0700

    Save point for convex optimization homework :(

---
+++ micro_g/launch/micro_g.launch.py
@@ -0,0 +1,63 @@
+# Copyright 2023, Micro-G Dev Team
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from launch import LaunchDescription
+from launch.conditions import IfCondition
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description() -> LaunchDescription:
+    """Generate a launch description to run the system.
+
+    Returns:
+        The launch description for the micro-g system.
+    """
+    args = [
+        DeclareLaunchArgument(
+            "use_sim",
+            default_value=False,
+            description="Launch the micro-g simulation environment.",
+        )
+    ]
+
+    use_sim = LaunchConfiguration("use_sim")
+
+    includes = [
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [
+                    PathJoinSubstitution(
+                        [
+                            FindPackageShare("micro_g"),
+                            "launch",
+                            "sim.launch.py",
+                        ]
+                    )
+                ]
+            ),
+            condition=IfCondition(use_sim),
+        ),
+    ]
+
+    return LaunchDescription(args + includes)
