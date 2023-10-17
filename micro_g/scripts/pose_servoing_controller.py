9ddcd982c125bf522c6368bb06f69e0e0e13ed1c
Charles Dawson <charles.dwsn@gmail.com>
2023-10-17 01:09:48 -0400

    Pose servoing controller now works! moving_time needs to be long enough or IK will fail

--- micro_g/scripts/pose_servoing_controller.py
+++ micro_g/scripts/pose_servoing_controller.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # Copyright 2023, Micro-G Dev Team
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -17,13 +18,18 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+import argparse
+import sys
 import time
 
 import numpy as np
+import rclpy
 import tf2_ros
+import transforms3d
 from geometry_msgs.msg import PoseStamped
 from interbotix_common_modules.angle_manipulation import angle_manipulation as ang
 from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
+from rclpy.utilities import remove_ros_args
 from tf2_geometry_msgs import do_transform_pose
 
 
@@ -32,13 +38,12 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
     Tracks a published end effector pose.
     """
 
-    control_update_rate: float = 50
-
     def __init__(
         self,
         robot_model: str,
         robot_name: str,
-        control_update_rate: float,
+        control_update_rate: float = 10.0,
+        moving_time: float = 1.0,
         xs_args=None,
     ):
         """
@@ -48,6 +53,7 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
             robot_model: the robot model
             robot_name: the robot name
             control_update_rate: the rate at which the controller is updated (Hz).
+            moving_time: the duration in which all movements should take place (s).
             xs_args: Extra arguments to be passed to InterbotixManipulatorXS. Defaults
                 to None.
         """
@@ -56,10 +62,11 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
             self,
             robot_model=robot_model,
             robot_name=robot_name,
-            moving_time=0.2,
-            accel_time=0.1,
             start_on_init=True,
+            moving_time=moving_time,
+            accel_time=moving_time / 2.0,
             args=xs_args,
+            node_name="pose_servoing",
         )
 
         # Set the controller update rate
@@ -72,17 +79,18 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
         #   - b = body frame, fixed to the end effector
         #   - d = desired frame for the end effector
         self.T_sy = np.identity(4)
+        self.T_sd = self.arm.get_ee_pose_command()
         self.update_frames()
 
         # Initialize the tf2 buffer and listener
         self.tf_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self.core)
         # Set the target frame for transformation
         self.target_frame = f"{robot_name}/base_link"
 
         # Create a subscription to the pose that we will track
         self.core.create_subscription(
-            PoseStamped, "robot_name/desired_eef_pose", self.desired_pose_callback, 10
+            PoseStamped, "desired_eef_pose", self.desired_pose_callback, 10
         )
         time.sleep(0.5)
         self.core.get_logger().info("Ready to receive servoing commands.")
@@ -113,21 +121,24 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
             )
 
             # Transform the pose using the received transform
-            transformed_pose = do_transform_pose(msg, transform)
-            position = transformed_pose.pose.position
-            orientation = transformed_pose.pose.orientation
+            transformed_pose = do_transform_pose(msg.pose, transform)
+            position = transformed_pose.position
+            position = np.array([position.x, position.y, position.z])
+            orientation = transformed_pose.orientation
+            orientation = transforms3d.quaternions.quat2mat(
+                [orientation.w, orientation.x, orientation.y, orientation.z]
+            )
 
             # Save the position and orientation into the homogeneous transform
-            rpy = np.array([orientation.x, orientation.y, orientation.z])
-            np.array([position.x, position.y, position.z])
-            self.T_sd[:3, :3] = ang.euler_angles_to_rotation_matrix(rpy)
+            self.T_sd[:3, :3] = orientation
             self.T_sd[:3, 3] = position
-            self.core.get_logger().info(f"T_sd:\n{self.T_sd}")
+            self.core.get_logger().info(f"Received new desired pose T_sd:\n{self.T_sd}")
 
         except Exception as e:
             self.core.get_logger().error(
                 f"Error transforming pose to {self.target_frame}: {e}"
             )
+            raise (e)
 
     def controller(self):
         """Run the controller and send commands to the robot."""
@@ -137,17 +148,40 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
 
         # Project the desired pose into the plane of the arm by zeroing the y coordinate
         T_yd[1, 3] = 0.0
-
-        # Convert back to the fixed base frame and send to the arm
+        # Convert back to space frame
         T_sd = np.dot(self.T_sy, T_yd)
+
+        # Send to the robot
         _, success = self.arm.set_ee_pose_matrix(
             T_sd=T_sd,
             custom_guess=self.arm.get_joint_commands(),
             execute=True,
-            moving_time=0.2,
-            accel_time=0.1,
             blocking=False,
         )
 
         if not success:
-            self.core.get_logger().warn(f"Desired pose not reachable!\n{T_sd}")
+            self.core.get_logger().warn(f"Desired pose not reachable!\n{self.T_sd}")
+
+    def start_robot(self):
+        """Run the controller until ROS triggers a shutdown."""
+        try:
+            self.start()
+            while rclpy.ok():
+                self.controller()
+                self.rate.sleep()
+        except KeyboardInterrupt:
+            self.shutdown()
+
+
+if __name__ == "__main__":
+    # Get command line arguments
+    p = argparse.ArgumentParser()
+    p.add_argument("--robot_model", required=True)
+    p.add_argument("--robot_name", required=True)
+    p.add_argument("args", nargs=argparse.REMAINDER)
+
+    command_line_args = remove_ros_args(args=sys.argv)[1:]
+    ros_args = p.parse_args(command_line_args)
+
+    bot = XSArmPoseServoingController(ros_args.robot_model, ros_args.robot_name)
+    bot.start_robot()
