abea9743a8e420274fda7307756eca76417f51b1
Charles Dawson <charles.dwsn@gmail.com>
2023-12-06 18:39:55 +0000

    Move IK calls outside of the control loop, add random restarts, to make more reliable IK

--- micro_g_controllers/micro_g_controllers/pose_servoing_controller.py
+++ micro_g_controllers/micro_g_controllers/pose_servoing_controller.py
@@ -42,7 +42,7 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
         robot_model: str,
         robot_name: str,
         control_update_rate: float = 20.0,
-        moving_time: float = 0.25,
+        moving_time: float = 0.5,
         xs_args=None,
     ):
         """
@@ -74,50 +74,63 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
         )
 
         # Declare ROS parameters
-        control_rate_desc = ParameterDescriptor(
-            type=ParameterType.PARAMETER_DOUBLE, description="Control update rate in Hz"
+        self.core.declare_parameters(
+            namespace="",
+            parameters=[
+                (
+                    "control_update_rate",
+                    control_update_rate,
+                    ParameterDescriptor(
+                        type=ParameterType.PARAMETER_DOUBLE,
+                        description="Control update rate in Hz",
+                    ),
+                ),
+                (
+                    "moving_time",
+                    moving_time,
+                    ParameterDescriptor(
+                        type=ParameterType.PARAMETER_DOUBLE,
+                        description="Moving time in seconds",
+                    ),
+                ),
+                (
+                    "kp",
+                    0.8,
+                    ParameterDescriptor(
+                        type=ParameterType.PARAMETER_DOUBLE,
+                        description="Proportional gain for joint tracking controller",
+                    ),
+                ),
+                (
+                    "replanning_attempts",
+                    5,
+                    ParameterDescriptor(
+                        type=ParameterType.PARAMETER_INTEGER,
+                        description="Number of times to attempt re-planning",
+                    ),
+                ),
+                (
+                    "timeout",
+                    1.0,
+                    ParameterDescriptor(
+                        type=ParameterType.PARAMETER_DOUBLE,
+                        description="Wait this long after getting a target before moving home (s)",
+                    ),
+                ),
+            ],
         )
-        moving_time_desc = ParameterDescriptor(
-            type=ParameterType.PARAMETER_DOUBLE, description="Moving time in seconds"
-        )
-        replanning_attempts_desc = ParameterDescriptor(
-            type=ParameterType.PARAMETER_INTEGER,
-            description="Number of times to attempt re-planning",
-        )
-        home_x_desc = ParameterDescriptor(
-            type=ParameterType.PARAMETER_DOUBLE,
-            description="x coordinate of home position (m)",
-        )
-        home_z_desc = ParameterDescriptor(
-            type=ParameterType.PARAMETER_DOUBLE,
-            description="x coordinate of home position (m)",
-        )
-        timeout_desc = ParameterDescriptor(
-            type=ParameterType.PARAMETER_DOUBLE,
-            description="Wait this long after getting a target before moving home (s)",
-        )
-        self.core.declare_parameter(
-            "control_update_rate", 20.0, descriptor=control_rate_desc
-        )
-        self.core.declare_parameter("moving_time", 0.5, descriptor=moving_time_desc)
-        self.core.declare_parameter(
-            "replanning_attempts", 5, descriptor=replanning_attempts_desc
-        )
-        self.core.declare_parameter("home_x", 0.1, descriptor=home_x_desc)
-        self.core.declare_parameter("home_z", 0.2, descriptor=home_z_desc)
-        self.core.declare_parameter("timeout", 1.0, descriptor=timeout_desc)
 
         # Get ROS parameters
         control_update_rate = self.core.get_parameter("control_update_rate").value
         moving_time = self.core.get_parameter("moving_time").value
+        self.kp = self.core.get_parameter("kp").value
         self.replanning_attempts = self.core.get_parameter("replanning_attempts").value
-        self.home = np.array([self.core.get_parameter("home_x").value, 0.0, self.core.get_parameter("home_z").value])
+        self.home = [0.0, -1.494097352027893, 0.9418642520904541, 0.5338253378868103]
         self.timeout = self.core.get_parameter("timeout").value
         self.last_target_time = time.time()
 
-        # Set the controller update rate
-        self.control_update_rate = control_update_rate
-        self.rate = self.core.create_rate(self.control_update_rate)
+        # Initialize the gripper state
+        self.gripper.release(delay=0.0)
 
         # Update the moving time (and set acceleration time to half of moving time)
         self.arm.set_trajectory_time(moving_time, moving_time / 2.0)
@@ -128,9 +141,11 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
         #   - y = yaw frame, same as s but rotated about z by the current waist angle
         #   - b = end effector body frame (tool frame)
         #   - d = desired frame for the end effector
-        self.T_sy = np.identity(4)
-        self.T_sd = self.arm.get_ee_pose_command()
+        self.T_sy = np.eye(4)
         self.update_frames()
+
+        # Define the joints to track
+        self.desired_joint_positions = self.home
 
         # Initialize the tf2 buffer and listener
         self.tf_buffer = tf2_ros.Buffer()
@@ -145,8 +160,11 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
             self.desired_pose_callback,
             qos_profile_system_default,
         )
-        time.sleep(0.5)
         self.core.get_logger().info("Ready to receive servoing commands.")
+
+        # Set the controller update rate
+        self.control_update_rate = control_update_rate
+        self.core.create_timer(1.0 / self.control_update_rate, self.update)
 
     def update_frames(self):
         """
@@ -185,12 +203,34 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
             )
 
             # Save the position and orientation into the homogeneous transform
-            self.T_sd[:3, :3] = orientation
-            self.T_sd[:3, 3] = position
-            self.core.get_logger().debug(
-                f"Received new desired pose T_sd:\n{self.T_sd}"
-            )
-            self.last_target_time = time.time()
+            T_sd = np.eye(4)
+            T_sd[:3, :3] = orientation
+            T_sd[:3, 3] = position
+            self.core.get_logger().debug(f"Received new desired pose T_sd:\n{T_sd}")
+
+            # Project the pose into the plane of the arm by zeroing the y coordinate
+            T_ys = np.linalg.inv(self.T_sy)
+            T_yd = np.dot(T_ys, T_sd)
+            y_distance = T_yd[1, 3]
+            T_yd[1, 3] = 0.0
+            T_sd = np.dot(self.T_sy, T_yd)
+
+            # If the target is too far away along the y axis, wait for the linear axis
+            # controller to move the robot closer
+            if abs(y_distance) > 0.1:
+                self.core.get_logger().warn(
+                    f"Target is too far away along y axis ({y_distance} m); waiting for linear axis controller to move closer."
+                )
+                self.desired_joint_positions = self.home
+                return
+
+            # Solve IK to get the joint angles that track this pose
+            theta_list, success = self.get_joints_for_eef_pose(T_sd)
+            if not success:
+                self.core.get_logger().warn("Desired pose not reachable!")
+            else:
+                self.desired_joint_positions = theta_list
+                self.last_target_time = time.time()
 
         except Exception as e:
             self.core.get_logger().error(
@@ -198,72 +238,63 @@ class XSArmPoseServoingController(InterbotixManipulatorXS):
             )
             raise (e)
 
+    def get_joints_for_eef_pose(self, T_sd):
+        """Use IK to get joint angles that achieve the desired end effector pose."""
+        # Generate a list of initial guesses around the current joint angles
+        self.arm.capture_joint_positions()
+        joint_angles = np.array(self.arm.get_joint_commands())
+        initial_guesses = np.random.normal(
+            joint_angles,
+            scale=np.pi / 4.0,
+            size=(self.replanning_attempts, joint_angles.size),
+        )
+        # Start with the current joint angles as the first guess
+        initial_guesses = np.vstack([joint_angles, initial_guesses])
+
+        # Try each initial guess until we find one that works or run out of guesses
+        for initial_guess in initial_guesses:
+            theta_list, success = self.arm.set_ee_pose_matrix(
+                T_sd,
+                initial_guess,
+                execute=False,
+            )
+
+            if success:
+                return theta_list, success
+
+        return None, False
+
     def update(self):
         """Run the controller and send commands to the robot."""
         self.core.get_logger().info("Updating pose servoing controller.")
 
         # If we haven't received a target in a while, move home
         if time.time() - self.last_target_time > self.timeout:
-            self.core.get_logger().info(f"No target received in {time.time() - self.last_target_time} s; moving home.")
-            self.T_sd[:3, 3] = self.home
+            self.core.get_logger().info(
+                f"No target received in {time.time() - self.last_target_time} s; moving home."
+            )
+            self.desired_joint_positions = self.home
             self.gripper.grasp(delay=0.0)
         else:
             self.gripper.release(delay=0.0)
 
-        # Convert the desired pose into the arm-aligned base frame
-        T_ys = np.linalg.inv(self.T_sy)
-        T_yd = np.dot(T_ys, self.T_sd)
-
-        # Project the desired pose into the plane of the arm by zeroing the y coordinate
-        T_yd[1, 3] = 0.0
-
-        # Get the relative x, y, z, roll, pitch, yaw between the current pose and T_sd
-        T_sb = self.arm.get_ee_pose_command()
-        T_yb = np.dot(T_ys, T_sb)
-        position_offset = T_yd[:3, 3] - T_yb[:3, 3]
-        rpy_yb = np.array(ang.rotation_matrix_to_euler_angles(T_yb[:3, :3]))
-        rpy_yd = np.array(ang.rotation_matrix_to_euler_angles(T_yd[:3, :3]))
-        rpy_offset = rpy_yd - rpy_yb
-
-        # Set the target to be 70% of the way to the goal, to avoid overshoot
-        position_offset *= 0.7
-        rpy_offset *= 0.7
-
-        # Plan and execute. If planning fails (due to the pose not being reachable),
-        # re-plan with a nearer goal until we succeed or a maximum number of planning
-        # attempts are made.
-        success = False
-        attempts = 0
-        while not success and attempts < self.replanning_attempts:
-            attempts += 1
-            success = self.arm.set_ee_cartesian_trajectory(
-                x=position_offset[0],
-                y=0.0,
-                z=position_offset[2],
-                roll=rpy_offset[0],
-                pitch=rpy_offset[1],
-                yaw=0.0,
-            )
-
-            # Adjust goal to be nearer if we need to replan
-            if not success:
-                position_offset *= 0.5
-                rpy_offset *= 0.5
-                self.core.get_logger().info("Plan not found; replanning!")
-            else:
-                self.core.get_logger().info("Plan found! Executing!")
-
-        if not success:
-            self.core.get_logger().warn(f"Desired pose not reachable!\n{self.T_sd}")
+        # Track the desired joint positions
+        self.arm.capture_joint_positions()
+        q_current = np.array(self.arm.get_joint_commands())
+        q_desired = np.array(self.desired_joint_positions)
+        q_setpoint = q_current + (q_desired - q_current) * self.kp
+        self.core.get_logger().info(f"q_current: {q_current}")
+        self.core.get_logger().info(f"q_setpoint: {q_setpoint}")
+        self.arm.set_joint_positions(
+            q_setpoint.tolist(),
+            blocking=False,
+        )
 
     def start_robot(self):
         """Run the controller until ROS triggers a shutdown."""
         try:
             self.start()
-            time.sleep(1.0)  # wait a bit for the robot thread to start
-            while rclpy.ok():
-                self.update()
-                self.rate.sleep()
+            rclpy.spin(self.core)
         except KeyboardInterrupt:
             self.shutdown()
 
